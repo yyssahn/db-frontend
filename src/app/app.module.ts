aba187b73382642b4faaffe815b9d9d5700d7fc4
Kyle <yyssahn@hotmail.com>
2017-07-29 10:25:19 -0700

    first commit with some bare structure and recycled code

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { AppComponent }  from './app.component';
+import { DuberFormComponent } from './duber-form.component';
+
+@NgModule({
+  imports:      [
+  BrowserModule,
+  HttpModule,
+  ReactiveFormsModule
+  ],
+  declarations: [
+  AppComponent,
+  DuberFormComponent
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/duber-form.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Http, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Component({
+  selector:'db-form',
+  templateUrl: './duber-form.component.html',
+  styleUrls: ['./duber-form.component.css']
+})
+
+
+export class DuberFormComponent {
+  duberForm;
+  constructor(private formBuilder: FormBuilder, private http: Http){
+  }
+
+  ngOnInit(){
+    this.duberForm = this.formBuilder.group({
+      zipcode: this.formBuilder.control('',Validators.compose([
+      Validators.required,
+      Validators.pattern('\\d{5}'),
+
+      ])),
+      budget: this.formBuilder.control('50',Validators.compose([
+      Validators.required,
+      Validators.pattern('[\\d]+')
+      ]))
+    });
+
+
+  }
+
+
+  onSubmit(value){
+      console.log("sending request to ");
+      let params = new URLSearchParams();
+      params.set('zipcode', value.zipcode);
+      params.set('budget', value.budget);
+      this.http.get('http://127.0.0.1:3000/search/json',{search:params}).toPromise().then(result=>console.log(result));
+  }
+
+}
